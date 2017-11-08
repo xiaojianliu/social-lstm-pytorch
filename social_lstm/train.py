f0da7408d3adb7ab87598bb4bab5cb905525b5a6
hesl <N1701420F@e.ntu.edu.sg>
2017-11-08 15:41:56 +0800

    training my data

--- social_lstm/train.py
+++ social_lstm/train.py
@@ -64,7 +64,7 @@ def main():
     parser.add_argument('--grid_size', type=int, default=4,
                         help='Grid size of the social grid')
     # The leave out dataset
-    parser.add_argument('--leaveDataset', type=int, default=1,
+    parser.add_argument('--leaveDataset', type=int, default=4,
                         help='The dataset index to be left out in training')
     # Lambda regularization parameter (L2)
     parser.add_argument('--lambda_param', type=float, default=0.0001,
